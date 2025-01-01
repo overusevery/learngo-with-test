c4e34145406d03495c67b936639f231eee759fd3
tomohiro.yamaoka <overusevery@gmail.com>
2025-01-01 09:46:32 +0900

    scaffold:red:get players test

---
+++ internal/handler/playerHandler/playerHandler.go
@@ -0,0 +1,5 @@
+package playerhandler
+
+import "net/http"
+
+func PlayerHandler(w http.ResponseWriter, r *http.Request) {}
---
+++ internal/handler/playerHandler/playerHandler_test.go
@@ -0,0 +1,23 @@
+package playerhandler
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestPlayerHandler(t *testing.T) {
+	t.Run("returns Pepper's score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
+		response := httptest.NewRecorder()
+
+		PlayerHandler(response, request)
+
+		got := response.Body.String()
+		want := "20"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+}
